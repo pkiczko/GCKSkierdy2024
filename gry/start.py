9587acfb150944114f5e71349ba4bf957fb40d49
pkiczko <pkiczko@gmail.com>
2024-05-14 18:43:53 +0200

    do dokończenia

--- gry/start.py
+++ gry/start.py
@@ -34,6 +34,39 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # Run until the user asks to quit
 running = True
 y = 0
+
+# Define a Player object by extending pygame.sprite.Sprite
+# The surface drawn on the screen is now an attribute of 'player'
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 0, 255))   #różowy
+        self.rect = self.surf.get_rect()
+
+
+player = Player()
+
+# Move the sprite based on user keypresses
+def update(self, pressed_keys):
+    if pressed_keys[K_UP]:
+        self.rect.move_ip(0, -5)
+    if pressed_keys[K_DOWN]:
+        self.rect.move_ip(0, 5)
+    if pressed_keys[K_LEFT]:
+        self.rect.move_ip(-5, 0)
+    if pressed_keys[K_RIGHT]:
+        self.rect.move_ip(5, 0)
+    # Keep player on the screen
+    if self.rect.left < 0:
+        self.rect.left = 0
+    if self.rect.right > SCREEN_WIDTH:
+        self.rect.right = SCREEN_WIDTH
+    if self.rect.top <= 0:
+        self.rect.top = 0
+    if self.rect.bottom >= SCREEN_HEIGHT:
+        self.rect.bottom = SCREEN_HEIGHT
+
 while running:
 
     # Did the user click the window close button?
@@ -53,15 +86,23 @@ while running:
 
     # Draw a solid blue circle in the center
     pygame.draw.circle(screen, (255, 0, 0), (250,y), 70)
-    
+
     # Create a surface and pass in a tuple containing its length and width
     surf = pygame.Surface((50, 50))
 
     surf.fill((0, 0, 0))
     rect = surf.get_rect()
     
+    # Get all the keys currently pressed
+    pressed_keys = pygame.key.get_pressed()
+
+    # Update the player sprite based on user keypresses
+    player.update(pressed_keys)
+
+    #narysuj gracza po środku ekranu
+    screen.blit(player.surf, player.rect)
     # This line says "Draw surf onto the screen at the center"
-    screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    #screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
     pygame.display.flip()
 
 # Done! Time to quit.
